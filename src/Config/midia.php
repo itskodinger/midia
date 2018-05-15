5e1578a65e7fb9037a5944b5e109d0a6ad21f6ef
Daniel Ravenshorst <daniel@onlinesquare.nl>
2018-05-15 02:34:04 +0200

    Modifies midia.php: * Adds home_directory to storage path. * Adds home_directory_name to symlink (this case easier, otherwise create new symlink). * And more as example.

--- src/Config/midia.php
+++ src/Config/midia.php
@@ -1,8 +1,16 @@
 <?php
 return [
-	// Target diirectory
-	'directory' => storage_path('media'),
-	// For URL (e.g: http://base/media/filename.ext)
-	'directory_name' => 'media',
-	'prefix' => 'midia'
+    // Target directory
+    'directory' => storage_path('media'),
+    // For URL (e.g: http://base/media/filename.ext)
+    'directory_name' => 'media',
+    'prefix' => 'midia',
+    // Multiple target directories
+    'home_directory' => storage_path('media/site/home'),
+    'home_directory_name' => 'media/site/home',
+    '...._directory' => '',
+    '...._directory_name' => '',
+    'directory...' => '',
+    'directory_name...' => '',
+    // etc...
 ];
