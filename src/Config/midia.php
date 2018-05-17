b03bafd80251f456bcd5f39420d8534fc6497b59
captainspain <captainspain99@gmail.com>
2018-05-17 11:26:01 +0200

    Update midia.php
    Removes strange keys (as it was an example, but not very clean).

--- src/Config/midia.php
+++ src/Config/midia.php
@@ -1,16 +1,13 @@
 <?php
 return [
-    // Target directory
+    // DEFAULT Target directory
     'directory' => storage_path('media'),
     // For URL (e.g: http://base/media/filename.ext)
     'directory_name' => 'media',
     'prefix' => 'midia',
+    
     // Multiple target directories
     'home_directory' => storage_path('media/site/home'),
     'home_directory_name' => 'media/site/home',
-    '...._directory' => '',
-    '...._directory_name' => '',
-    'directory...' => '',
-    'directory_name...' => '',
     // etc...
 ];
