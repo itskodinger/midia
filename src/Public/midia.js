504e152a521566b0f35bb700b6544b6dacac6063
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2018-08-01 08:57:40 +0700

    add isinviewport function

--- src/Public/midia.js
+++ src/Public/midia.js
@@ -1,4 +1,12 @@
 (function($, window, i) {
+    $.fn.isInViewport = function() {
+        var elementTop = $(this).offset().top;
+        var elementBottom = elementTop + $(this).outerHeight();
+        var viewportTop = $(window).scrollTop();
+        var viewportBottom = viewportTop + $(window).height();
+        return elementBottom > viewportTop && elementTop < viewportBottom;
+    };
+
     $.fn.midia = function(options) {
         var options = $.extend({
             title: 'Midia',
